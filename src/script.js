40bee37cd7e814e80d1467484854c9919eb648e5
saabyer <salarclydexavier@gmail.com>
2024-04-16 14:33:48 +0800

    add

---
+++ src/script.js
@@ -0,0 +1,66 @@
+function redirectToSignup() {
+    // Set the URL of your signup page
+    var signupPageUrl = "signup.html"; // Replace with the actual path
+
+    // Redirect to the signup page
+    window.location.href = signupPageUrl;
+    console.log("Sucessfuly redirected to signup page");
+  }
+
+function redirectToHome() {
+    var signupPageUrl = "index.html";
+
+    window.location.href = signupPageUrl;
+    console.log("Suceessfuly redirected to home page");
+  }
+
+function redirectToLogin() {
+    var signupPageUrl = "login.html";
+
+    window.location.href = signupPageUrl;
+    console.log("Suceessfuly redirected to login page");
+  }
+
+function redirectToDownload() {
+    var signupPageUrl = "download.html";
+
+    window.location.href = signupPageUrl;
+    console.log("Suceessfuly redirected to download page");
+  }
+
+
+//Open file dialog
+function openFileDialog() {
+  // Trigger the click event on the hidden file input
+  document.getElementById('fileInput').click();
+}
+document.getElementById('fileInput').addEventListener('change', handleFileSelection);
+
+function handleFileSelection() {
+  // Access the selected file(s) using the files property
+  const selectedFile = document.getElementById('fileInput').files[0];
+  
+  if (!selectedFile) {
+      console.log('No file selected.');
+      return;
+  }
+  console.log('Selected file:', selectedFile.name);
+}
+
+// Save file dialog
+function saveFileDialog() {
+  // Trigger the click event on the hidden file input
+  document.getElementById('fileInput').click();
+}
+document.getElementById('fileInput').addEventListener('change', handleFileSelection);
+
+function handleFileSelection() {
+  // Access the selected file(s) using the files property
+  const selectedFile = document.getElementById('fileInput').files[0];
+  
+  if (!selectedFile) {
+      console.log('No file selected.');
+      return;
+  }
+  console.log('Selected file:', selectedFile.name);
+}
